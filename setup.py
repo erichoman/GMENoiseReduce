8022f8fce0c1b2f4e22393f8f692be47caf70699
erichoman <ejhoman92@gmail.com>
2024-01-19 15:41:53 +0000

    fix for packaging

--- setup.py
+++ setup.py
@@ -14,16 +14,8 @@ setup(
           'numpy'
       ],
   classifiers=[
-    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-
-    'Intended Audience :: Developers, Scientists',      # Define that your audience are developers
-    'Topic :: Noise Reduction',
-
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
   ],
 )
