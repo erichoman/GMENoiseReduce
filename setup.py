adacab4cd193ed221c459bfe568468cb1f7770ac
erichoman <ejhoman92@gmail.com>
2024-01-19 15:34:27 +0000

    Fixed Email

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Generalised Maximum Entropy white noise Elimination',   # Give a short description about your library
   author = 'Eric Homan',                   # Type in your name
-  author_email = 'ejhoman92"gmail.com',      # Type in your E-Mail
+  author_email = 'ejhoman92@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/erichoman/GMENoiseReduce',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['Noise', 'Noise-reduction', 'Noise removal'],   # Keywords that define your package best
